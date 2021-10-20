98f74497f704e57a1918e991caeed96328add810
Ashita Mahajan <ashitamahajandal@gmail.com>
2021-10-19 20:26:04 -0700

    billing portal

---
+++ app/controllers/webhooks_controller.rb
@@ -0,0 +1,52 @@
+class WebhooksController < ApplicationController
+  skip_before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token
+
+
+  protect_from_forgery except: :webhook
+
+  #def create
+  def create
+    payload = request.body.read
+    sig_header = request.env['HTTP_STRIPE_SIGNATURE']
+    event = nil
+
+
+    begin
+      event = Stripe::Webhook.construct_event(
+        payload, sig_header, "we_1JgRCTSBCiWiWvNcFM6PZRik"
+      )
+    rescue JSON::ParserError => e
+      status 400
+      return
+    rescue Stripe::SignatureVerificationError => e
+      # Invalid signature
+      puts "Signature error"
+      p e
+      return
+    end
+    
+    # Handle the event
+    case event.type
+    when 'customer.created'
+      customer = event.data.object
+      @user.update(stripe_customer_id: customer.id)
+      @user = User.find_by(email: customer.email)
+      
+    when 'checkout.session.completed'
+      session = event.data.object
+      @user = User.find_by(stripe_customer_id: session.customer)
+      @user.update(subscription_status: 'active')
+    when 'customer.subscription.updated', 'customer.subscription.deleted'
+      subscription = event.data.object
+      @user = User.find_by(stripe_customer_id: subscription.customer)
+      @user.update(
+        subscription_status: subscription.status,
+        plan: subscription.item.data[0].price.lookup_key,
+        )
+    end
+
+
+    render json: {message: 'success'}
+  end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   root "posts#index"
   get "pricing", to: "static_pages#pricing"
   resources :posts
-  
+  post "checkout/create", to: "checkout#create", as: "checkout_create"
+  post "billing_portal/create", to: "billing_portal#create", as: "billing_portal_create"
+  resources :webhooks, only: [:create]
 end
